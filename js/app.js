87b7c23078193450b89c029c52021ab67d2ba162
usigna <sylwia.fait@gmail.com>
2023-09-09 22:25:57 +0200

    Add code

---
+++ js/app.js
@@ -0,0 +1,72 @@
+function handleFirstTab(e) {
+  if (e.key === 'Tab') {
+    document.body.classList.add('user-is-tabbing');
+    window.removeEventListener('keydown', handleFirstTab);
+  }
+};
+
+window.addEventListener('keydown', handleFirstTab);
+
+const computerChoiceDisplay = document.getElementById('computer-choice');
+const userChoiceDisplay = document.getElementById('user-choice');
+const resultDisplay = document.getElementById('result');
+const possibleChoices = document.querySelectorAll('.btn');
+let userChoice;
+let computerChoice;
+let result;
+
+possibleChoices.forEach(possibleChoices => possibleChoices.addEventListener('click', (e) => {
+  userChoice = e.target.id;
+  userChoiceDisplay.innerHTML = `<img src="img/${userChoice}.svg" alt="${userChoice}" />`;
+  generateComputerChoice();
+  getResult();
+}))
+
+function generateComputerChoice() {
+  const randomNumber = Math.floor(Math.random() * possibleChoices.length) + 1;
+
+  if (randomNumber === 1) {
+    computerChoice = 'rock';
+  }
+
+  if (randomNumber === 2) {
+    computerChoice = 'paper';
+  }
+
+  if (randomNumber === 3) {
+    computerChoice = 'scissors';
+  }
+  computerChoiceDisplay.innerHTML = `<img src="img/${computerChoice}.svg" alt="${computerChoice}" />`;
+}
+
+function getResult() {
+  if (computerChoice === userChoice) {
+    result = 'its a draw!';
+  }
+
+  if (computerChoice === 'rock' && userChoice === 'paper') {
+    result = 'you win!';
+  }
+
+  if (computerChoice === 'rock' && userChoice === 'scissors') {
+    result = 'you lose!';
+  }
+
+  if (computerChoice === 'paper' && userChoice === 'scissors') {
+    result = 'you win!';
+  }
+
+  if (computerChoice === 'paper' && userChoice === 'rock') {
+    result = 'you lose!';
+  }
+
+  if (computerChoice === 'scissors' && userChoice === 'rock') {
+    result = 'you win!';
+  }
+
+  if (computerChoice === 'scissors' && userChoice === 'paper') {
+    result = 'you lose!';
+  }
+
+  resultDisplay.textContent = result;
+}
